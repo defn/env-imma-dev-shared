a915d2b404cc904f0a491086ed8a42ebd81c6f85
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-14 20:47:16 -0700

    reuse env vars

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-variable "bucket_remote_state" {}
-variable "context_org" {}
-variable "context_env" {}
-
-variable "az_count" {}
-
 variable "nat_cidrs" {}
 variable "vpn_cidrs" {}
 variable "jump_cidrs" {}
